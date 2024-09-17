99c4904bdf94a6cd7223d75166bf5e4cc5abbfde
daveward72 <dwward1972@gmail.com>
2024-09-16 20:51:10 -0500

    add line drawing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from window import *
+from graphics import *
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(200, 500)
+    p2 = Point(600, 100)
+    l1 = Line(p1, p2)
+    win.draw_line(l1, 'green')
+
+    p3 = Point(400, 100)
+    p4 = Point(500, 500)
+    l2 = Line(p3, p4)
+    win.draw_line(l2, 'red')
+
     win.wait_for_close()
 
 main()
--- src/window.py
+++ src/window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from graphics import *
 
 class Window:
     def __init__(self, width, height):
@@ -28,3 +29,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
