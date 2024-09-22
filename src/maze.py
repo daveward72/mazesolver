49ff969c44a43323cb3e2b23ae04da9dbb80bbf1
daveward72 <dwward1972@gmail.com>
2024-09-22 10:20:04 -0500

    break walls in maze

--- src/maze.py
+++ src/maze.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 from cell import *
 
@@ -12,6 +13,7 @@ class Maze:
         cell_size_x,
         cell_size_y,
         win = None,
+        seed = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -21,8 +23,12 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
 
+        if seed != None:
+            random.seed(seed)    
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
         self._cells = []
@@ -64,3 +70,40 @@ class Maze:
         bottom_right_cell.has_bottom_wall = False
         self._draw_cell(self._num_cols-1, self._num_rows-1)
 
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while True:
+            to_visit = []
+            for dir in dirs:
+                new_i = i + dir[0]
+                new_j = j + dir[1]
+                if new_i >= 0 and new_i < self._num_cols and new_j >= 0 and new_j < self._num_rows and not self._cells[new_i][new_j].visited:
+                    to_visit.append((new_i, new_j))
+
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+                
+            random_dir = random.choice(to_visit)
+            self._break_walls(i, j, random_dir[0], random_dir[1])
+            self._break_walls_r(random_dir[0], random_dir[1])
+
+    def _break_walls(self, i1, j1, i2, j2):
+        if i1 < i2:
+            self._cells[i1][j1].has_right_wall = False
+            self._cells[i2][j2].has_left_wall = False
+        elif i1 > i2:
+            self._cells[i1][j1].has_left_wall = False
+            self._cells[i2][j2].has_right_wall = False
+        elif j1 < j2:
+            self._cells[i1][j1].has_bottom_wall = False
+            self._cells[i2][j2].has_top_wall = False
+        elif j2 < j1:
+            self._cells[i1][j1].has_top_wall = False
+            self._cells[i2][j2].has_bottom_wall = False
+
+        self._draw_cell(i1, j1)
+        self._draw_cell(i2, j2)
